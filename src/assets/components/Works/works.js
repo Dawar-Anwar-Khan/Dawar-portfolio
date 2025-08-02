a5e6d66f5ae32d97e1c7d42cdb5fae028c08c587
Dawar <kdawar0102@gmail.com>
2025-08-02 18:08:36 +0530

    links added

--- src/assets/components/Works/works.js
+++ src/assets/components/Works/works.js
@@ -1,32 +1,32 @@
 export default [
     {
-        title: "Planora",
-        context: "The ultimate AI-powered planner! Use natural voice commands to create, organize, and export your schedule as an ICS file. Simplify planning with seamless voice-to-calendar integration.",
-        link:"#",
+        title: "The Shoe Company",
+        context: "A sleek, responsive landing page built with HTML, CSS, and JavaScript, designed to showcase a fictional footwear brand. The site features a clean UI, smooth scroll interactions, and mobile responsiveness. Perfect for presenting a product-based concept with style and clarity.",
+        link:"https://theshoecompany0.netlify.app/",
         code:""
     },
     {
-        title: "Batch",
-        context: "The ultimate AI-powered planner! Use natural voice commands to create, organize, and export your schedule as an ICS file. Simplify planning with seamless voice-to-calendar integration.",
-        link:"",
+        title: "Close to the world",
+        context: "An interactive and responsive web app that allows users to explore detailed information about countries around the world. Built with HTML, CSS, and ReactJS, the app fetches real-time data from the REST Countries API and displays it in a clean card-based UI.",
+        link:"https://close-to-the-world-app.netlify.app/",
         code:""
     },
     {
-        title: "SearchMe",
-        context: "The ultimate AI-powered planner! Use natural voice commands to create, organize, and export your schedule as an ICS file. Simplify planning with seamless voice-to-calendar integration.",
-        link:"",
+        title: "Focus on today",
+        context: "A clean and minimal productivity web app built to help users stay focused on their daily priorities. Designed with a user-first interface using HTML, CSS, and JavaScript, this tool lets you set your main goal for the day, mark it as complete, and reset it every new day.",
+        link:"https://focus-on-today-ak.netlify.app",
         code:""
     },
     {
-        title: "Task Flow",
-        context: "The ultimate AI-powered planner! Use natural voice commands to create, organize, and export your schedule as an ICS file. Simplify planning with seamless voice-to-calendar integration.",
-        link:"",
+        title: "The Shopper App",
+        context: "A sleek and responsive e-commerce frontend interface built using ReactJS and Tailwind CSS. This project simulates a shopping experience with a focus on clean design, fast performance, and intuitive user flow. It serves as a great showcase of frontend skills and UI design principles",
+        link:"https://the-shopper-app.netlify.app",
         code:""
     },
     {
-        title: "Task ",
-        context: "The ultimate AI-powered planner! Use natural voice commands to create, organize, and export your schedule as an ICS file. Simplify planning with seamless voice-to-calendar integration.",
-        link:"",
+        title: "Pure Tunes",
+        context: "A visually engaging and responsive music streaming app interface built with VanillaJS and HTML, CSS. PureTunes mimics the look and feel of modern music platforms, providing a smooth user experience with clean layouts, rich visuals, and interactive elements.",
+        link:"https://puretunes.netlify.app",
         code:""
     },
 ]
